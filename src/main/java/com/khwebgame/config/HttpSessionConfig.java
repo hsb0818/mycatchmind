1ebc2c74ba675032f0267bbc6479cd24c0af11ec
Sungbin Hong <hsb0818@gmail.com>
2018-01-29 00:20:24 +0900

    session with mysql

---
+++ src/main/java/com/khwebgame/config/HttpSessionConfig.java
@@ -0,0 +1,10 @@
+package com.khwebgame.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@Configuration
+@EnableJdbcHttpSession
+public class HttpSessionConfig {
+
+}
